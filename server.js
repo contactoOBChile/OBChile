bcf3fb60be435e2d465f940109d1b20a7a7cec63
contactoOBChile <contacto@officebankingchile.info>
2026-05-31 22:52:36 -0400

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ const express = require("express");
 const cors = require("cors");
 const fs = require("fs");
 const path = require("path");
-const puppeteer = require("puppeteer");
 // Si tu Node es <18, instala node-fetch y descomenta:
 // const fetch = require("node-fetch");
 
